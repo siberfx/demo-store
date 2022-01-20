5fa96fadcf4be2c2a6ea6fbefddfcb9b21dae2bd
Alec Ritson <hello@itsalec.co.uk>
2022-01-20 14:39:44 +0000

    Start navigation

---
+++ app/Http/Livewire/Components/Navigation.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Livewire\Components;
+
+use GetCandy\Models\Collection;
+use Livewire\Component;
+
+class Navigation extends Component
+{
+    public function getCollectionsProperty()
+    {
+        return Collection::get()->toTree();
+    }
+
+    public function render()
+    {
+        return view('livewire.components.navigation');
+    }
+}
