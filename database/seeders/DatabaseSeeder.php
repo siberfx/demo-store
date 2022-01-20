12d02dc79ea7173469b214ce55183c0df00f749b
Alec Ritson <hello@itsalec.co.uk>
2022-01-20 14:04:25 +0000

    Fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(CollectionSeeder::class);
-        // $this->call(AttributeSeeder::class);
+        $this->call(CollectionSeeder::class);
+        $this->call(AttributeSeeder::class);
         $this->call(ProductSeeder::class);
     }
 }
