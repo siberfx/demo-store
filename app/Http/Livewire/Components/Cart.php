80aca35a043e26debc872c0266476f6a30758989
Alec Ritson <hello@itsalec.co.uk>
2022-01-21 12:52:53 +0000

    Set up initial cart

--- app/Http/Livewire/Components/Cart.php
+++ app/Http/Livewire/Components/Cart.php
@@ -7,85 +7,20 @@ use Livewire\Component;
 
 class Cart extends Component
 {
-    /**
-     * The editable cart lines.
-     *
-     * @var array
-     */
-    public array $lines;
 
-    protected $listeners = [
-        'add-to-cart' => 'mapLines',
-    ];
-
-    /**
-     * {@inheritDoc}
-     */
     public function mount()
     {
-        $this->mapLines();
+        // dd($this->cart->subTotal);
     }
 
-    /**
-     * Get the current cart instance.
-     *
-     * @return \GetCandy\Managers\CartManager
-     */
     public function getCartProperty()
     {
-        return CartSession::current();
+        return CartSession::getCart();
     }
 
-    /**
-     * Return the cart lines from the cart.
-     *
-     * @return \Illuminate\Support\Collection
-     */
-    public function getCartLinesProperty()
+    public function getLinesProperty()
     {
-        return $this->cart->lines ?? collect();
-    }
-
-    /**
-     * Update the cart lines.
-     *
-     * @return void
-     */
-    public function updateLines()
-    {
-        CartSession::manager()->updateLines(
-            collect($this->lines)
-        );
-        $this->mapLines();
-    }
-
-    public function removeLine($id)
-    {
-        CartSession::manager()->removeLine($id);
-        $this->mapLines();
-    }
-
-    /**
-     * Map the cart lines.
-     *
-     * We want to map out our cart lines like this so we can
-     * add some validation rules and make them editable.
-     *
-     * @return void
-     */
-    public function mapLines()
-    {
-        $this->lines = $this->cartLines->map(function ($line) {
-            return [
-                'id' => $line->id,
-                'identifier' => $line->purchasable->getIdentifier(),
-                'quantity' => $line->quantity,
-                'description' => $line->purchasable->getDescription(),
-                'thumbnail' => $line->purchasable->getThumbnail(),
-                'option' => $line->purchasable->getOption(),
-                'sub_total' => $line->subTotal->formatted(),
-            ];
-        })->toArray();
+        return $this->cart->lines;
     }
 
     public function render()
--- resources/views/livewire/components/cart.blade.php
+++ resources/views/livewire/components/cart.blade.php
@@ -19,65 +19,16 @@
 
         <span class="block mt-1 text-xs font-medium">Cart</span>
     </button>
-    <div class="absolute right-0 z-50 p-4 bg-gray-900 rounded-b shadow w-96" x-show="linesVisible" x-on:click.away="linesVisible = false" x-cloak>
-
+    <div class="absolute right-0 z-50 w-64 p-4 bg-gray-900 rounded-b shadow" x-show="linesVisible">
         <div class="p-4 space-y-2 text-sm bg-white rounded">
-            @if($this->cart)
-                <div class="overflow-y-auto max-h-96">
-                    @forelse($lines as $index => $line)
-                        <div
-                            class="flex items-center py-2 space-x-4 border-t first:border-none"
-                            wire:key="line_{{ $line['id'] }}"
-                        >
-                            <div class="w-1/5">
-                                <img src="{{ $line['thumbnail'] }}" class="rounded">
-                            </div>
-                            <div class="grow">
-                                <strong>{{ $line['description'] }}</strong>
-                                <span class="block text-xs">{{ $line['identifier'] }} /  {{ $line['option'] }}</span>
-                                <div class="flex">
-                                    <div class="flex items-center mt-2 space-x-4">
-                                        <input
-                                            type="number"
-                                            class="w-1/3 p-2 text-xs border border-gray-900"
-                                            wire:model="lines.{{ $index }}.quantity"
-                                        />
-                                        <div class="grow">
-                                            @ {{ $line['sub_total'] }}
-                                        </div>
-                                    </div>
-                                    <div class="px-3">
-                                        <button type="button" wire:click="removeLine('{{ $line['id'] }}') }}">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                            </svg>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @empty
-                        <span class="text-xs">Go buy something :)</span>
-                    @endforelse
-                </div>
-                <div class="flex items-center justify-between pt-2 border-t">
-                    <strong>Sub Total</strong>
-                    {{ $this->cart->subTotal->formatted() }}
-                </div>
-            @else
+            @forelse($this->lines as $line)
+            @empty
                 <span class="text-xs">Go buy something :)</span>
-            @endif
+            @endforelse
+            <div class="flex items-center justify-between pt-2 border-t">
+                <strong>Sub Total</strong>
+                {{ $this->cart->subTotal->formatted() }}
+            </div>
         </div>
-        @if($this->cart)
-        <div class="flex justify-between pt-3">
-            <button
-                type="button"
-                class="px-3 py-2 text-sm text-center text-gray-200 uppercase border border-gray-600 rounded-md hover:border-white hover:bg-white hover:text-gray-900"
-                wire:click="updateLines"
-            >
-                Update Cart
-            </button>
-        </div>
-        @endif
     </div>
 </div>
