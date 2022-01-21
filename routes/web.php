007a67136b43c6cbe6676f228f249686981c2b71
Alec Ritson <hello@itsalec.co.uk>
2022-01-21 07:46:04 +0000

    Add in collection page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\CollectionPage;
 use App\Http\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', Home::class);
+
+Route::get('/collections/{slug}', CollectionPage::class)->name('collection.view');
