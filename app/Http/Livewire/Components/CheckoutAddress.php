eb4a1d0c997c2683779fdba6c8579b6a44a58a07
Alec Ritson <hello@itsalec.co.uk>
2022-01-27 15:28:02 +0000

    Flesh out ui for addresses

--- app/Http/Livewire/Components/CheckoutAddress.php
+++ app/Http/Livewire/Components/CheckoutAddress.php
@@ -39,6 +39,8 @@ class CheckoutAddress extends Component
      */
     public CartAddress $address;
 
+    public bool $shippingIsBilling = false;
+
     /**
      * Whether billing is the same as shipping.
      *
