a3edd35ae3b689deaead4237cff4786bfd889c02
Alec Ritson <hello@itsalec.co.uk>
2022-01-28 09:41:28 +0000

    Redirect if no cart

--- app/Http/Livewire/CheckoutPage.php
+++ app/Http/Livewire/CheckoutPage.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Livewire;
 
+use GetCandy\Facades\CartSession;
 use Livewire\Component;
+use Livewire\ComponentConcerns\PerformsRedirects;
 
 class CheckoutPage extends Component
 {
+    use PerformsRedirects;
+
+    public function mount()
+    {
+        if (!CartSession::current()) {
+            $this->redirect('/');
+        }
+    }
+
     public function render()
     {
         return view('livewire.checkout-page');
