f6fa79d7da64bcaf7d674fc2bbf08960b892cf5b
Alec Ritson <hello@itsalec.co.uk>
2022-01-28 13:52:39 +0000

    Initial shipping option

--- resources/views/livewire/components/shipping-options.blade.php
+++ resources/views/livewire/components/shipping-options.blade.php
@@ -2,7 +2,7 @@
     <div class="flex justify-between p-4 font-medium">
         <span class="text-xl">Shipping Option</span>
     </div>
-    @if($this->shippingAddress)
+    @if($this->hasShippingAddress)
         <div class="p-4 border-t">
             @foreach($this->shippingOptions as $option)
                 <label class="flex items-center w-full cursor-pointer">
