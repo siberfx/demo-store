d61d65bc74facc313edccecb3cabc67a0de279d6
Alec Ritson <hello@itsalec.co.uk>
2022-01-28 13:52:39 +0000

    Initial shipping option

--- app/Modifiers/ShippingModifier.php
+++ app/Modifiers/ShippingModifier.php
@@ -23,14 +23,5 @@ class ShippingModifier
                 taxClass: $taxClass
             )
         );
-
-        ShippingManifest::addOption(
-            new ShippingOption(
-                description: 'Express Delivery',
-                identifier: 'EXPDEL',
-                price: new Price(1000, $cart->currency, 1),
-                taxClass: $taxClass
-            )
-        );
     }
 }
