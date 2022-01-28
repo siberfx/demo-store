4755ac0200f12088cc88cccecba3ad5073600fae
Alec Ritson <hello@itsalec.co.uk>
2022-01-28 11:21:40 +0000

    Fix address refreshing and setting

--- app/Http/Livewire/CheckoutPage.php
+++ app/Http/Livewire/CheckoutPage.php
@@ -10,11 +10,20 @@ class CheckoutPage extends Component
 {
     use PerformsRedirects;
 
+    protected $listeners = [
+        'addressUpdated' => 'triggerAddressRefresh'
+    ];
+
     public function mount()
     {
         if (!CartSession::current()) {
             $this->redirect('/');
         }
+    }
+
+    public function triggerAddressRefresh()
+    {
+        $this->emit('refreshAddress');
     }
 
     public function render()
