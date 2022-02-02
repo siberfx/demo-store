8da3abbb6a363925ebcc854695829e831aa3b7d1
Alec Ritson <hello@itsalec.co.uk>
2022-02-02 12:11:13 +0000

    Add loading indicator

--- resources/views/partials/checkout/address.blade.php
+++ resources/views/partials/checkout/address.blade.php
@@ -121,11 +121,25 @@
     </div>
     <div class="flex justify-end w-full p-4 bg-gray-100">
         @if($step == $currentStep)
-            <button type="submit" wire:key="submit_btn" class="px-5 py-3 font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-500">
-                Continue
+            <button
+                type="submit"
+                wire:key="submit_btn"
+                class="px-5 py-3 font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-500"
+                wire:loading.attr="disabled"
+                wire:target="saveAddress"
+            >
+                <span wire:loading.remove wire:target="saveAddress">
+                    Save Address
+                </span>
+                <span wire:loading wire:target="saveAddress">
+                    <svg class="w-5 h-5 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                </span>
             </button>
         @else
-            <button type="button" wire:click.prevent="$set('currentStep', {{ $step }})" class="px-5 py-3 font-medium text-gray-600 rounded-lg bg-gray-50 hover:bg-gray-500">
+            <button type="button" wire:click.prevent="$set('currentStep', {{ $step }})" class="px-5 py-3 font-medium text-gray-600 bg-white rounded-lg hover:bg-gray-200">
                 Edit
             </button>
         @endif
