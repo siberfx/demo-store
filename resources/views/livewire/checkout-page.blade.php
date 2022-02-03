4aa63117e72133ff9b8187c902beb421e410c13a
Alec Ritson <hello@itsalec.co.uk>
2022-02-03 08:13:05 +0000

    Styling

--- resources/views/livewire/checkout-page.blade.php
+++ resources/views/livewire/checkout-page.blade.php
@@ -21,42 +21,46 @@
             ])
         </div>
         <div class="w-1/3 ml-12">
-            <h3 class="font-bold">Order Summary</h3>
+            <div class="sticky">
+                <h3 class="font-bold">Order Summary</h3>
 
-            <div class="mt-6">
-                @foreach($cart->lines as $line)
-                    <div wire:key="cart_line_{{ $line->id }}" class="flex items-center">
-                        <figure>
-                            <img src="{{ $line->purchasable->getThumbnail() }}" class="rounded w-14" />
-                        </figure>
-                        <div class="ml-4">
-                            <strong class="block">{{ $line->purchasable->getDescription() }}</strong>
-                            {{ $line->quantity }} @ {{ $line->subTotal->formatted() }}
+                <div class="mt-6">
+                    <div class="space-y-6">
+                    @foreach($cart->lines as $line)
+                        <div wire:key="cart_line_{{ $line->id }}" class="flex items-center">
+                            <figure>
+                                <img src="{{ $line->purchasable->getThumbnail() }}" class="rounded w-14" />
+                            </figure>
+                            <div class="ml-4">
+                                <strong class="block">{{ $line->purchasable->getDescription() }}</strong>
+                                {{ $line->quantity }} @ {{ $line->subTotal->formatted() }}
+                            </div>
                         </div>
+                    @endforeach
                     </div>
-                @endforeach
-                <div class="flex justify-between pt-4 mt-4 border-t">
-                    <strong class="block">Sub Total</strong>
-                    {{ $cart->subTotal->formatted() }}
-                </div>
-                @if($this->shippingOption)
                     <div class="flex justify-between pt-4 mt-4 border-t">
-                        <strong class="block">
-                            {{ $this->shippingOption->getDescription() }}
-                        </strong>
-                        {{ $this->shippingOption->getPrice()->formatted() }}
+                        <strong class="block">Sub Total</strong>
+                        {{ $cart->subTotal->formatted() }}
                     </div>
+                    @if($this->shippingOption)
+                        <div class="flex justify-between pt-4 mt-4 border-t">
+                            <strong class="block">
+                                {{ $this->shippingOption->getDescription() }}
+                            </strong>
+                            {{ $this->shippingOption->getPrice()->formatted() }}
+                        </div>
 
-                @endif
-                @foreach($cart->taxBreakdown as $tax)
+                    @endif
+                    @foreach($cart->taxBreakdown as $tax)
+                        <div class="flex justify-between pt-4 mt-4 border-t">
+                            <strong class="block">{{ $tax['rate']->name }}</strong>
+                            {{ $tax['total']->formatted() }}
+                        </div>
+                    @endforeach
                     <div class="flex justify-between pt-4 mt-4 border-t">
-                        <strong class="block">{{ $tax['rate']->name }}</strong>
-                        {{ $tax['total']->formatted() }}
+                        <strong class="block">Total</strong>
+                        {{ $cart->total->formatted() }}
                     </div>
-                @endforeach
-                <div class="flex justify-between pt-4 mt-4 border-t">
-                    <strong class="block">Total</strong>
-                    {{ $cart->total->formatted() }}
                 </div>
             </div>
         </div>
