8cef6598c5a1e854c9bc92b75ab273d1f4e5692f
Alec Ritson <hello@itsalec.co.uk>
2022-02-04 12:20:57 +0000

    Show collections

--- app/Http/Livewire/Home.php
+++ app/Http/Livewire/Home.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Livewire;
 
+use GetCandy\Models\Collection;
 use Livewire\Component;
 
 class Home extends Component
 {
+    /**
+     * Return the sale collection.
+     *
+     * @return void
+     */
+    public function getSaleCollectionProperty()
+    {
+        return Collection::find(6);
+    }
+
+    /**
+     * Return a random collection.
+     *
+     * @return void
+     */
+    public function getRandomCollectionProperty()
+    {
+        return Collection::inRandomOrder()->first();
+    }
+
     public function render()
     {
         return view('livewire.home');
