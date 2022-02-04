5203bee6c0265e64b8c48b3d930911a7e79f1fa7
Alec Ritson <hello@itsalec.co.uk>
2022-02-04 14:56:00 +0000

    Update ProductPageTest.php

--- tests/Unit/Http/Livewire/ProductPageTest.php
+++ tests/Unit/Http/Livewire/ProductPageTest.php
@@ -5,6 +5,8 @@ namespace Tests\Unit\Http\Livewire;
 use App\Http\Livewire\CollectionPage;
 use App\Http\Livewire\ProductPage;
 use GetCandy\Models\Collection;
+use GetCandy\Models\Currency;
+use GetCandy\Models\Price;
 use GetCandy\Models\Product;
 use GetCandy\Models\ProductVariant;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -22,10 +24,20 @@ class ProductPageTest extends TestCase
      */
     public function test_component_can_mount()
     {
+        $currency = Currency::factory()->create([
+            'default' => true,
+        ]);
+
         $product = Product::factory()
             ->hasUrls(1, [
                 'default' => true,
-            ])->has(ProductVariant::factory(), 'variants')
+            ])->has(ProductVariant::factory()->afterCreating(function ($variant) use ($currency) {
+                $variant->prices()->create(
+                    Price::factory()->make([
+                        'currency_id' => $currency->id,
+                    ])->getAttributes()
+                );
+            }), 'variants')
             ->create();
 
         Livewire::test(ProductPage::class, ['slug' => $product->urls->first()->slug])
@@ -39,10 +51,20 @@ class ProductPageTest extends TestCase
      */
     public function test_correct_product_is_loaded()
     {
+        $currency = Currency::factory()->create([
+            'default' => true,
+        ]);
+
         $product = Product::factory()
             ->hasUrls(1, [
                 'default' => true,
-            ])->has(ProductVariant::factory(), 'variants')
+            ])->has(ProductVariant::factory()->afterCreating(function ($variant) use ($currency) {
+                $variant->prices()->create(
+                    Price::factory()->make([
+                        'currency_id' => $currency->id,
+                    ])->getAttributes()
+                );
+            }), 'variants')
             ->create();
 
         Livewire::test(ProductPage::class, ['slug' => $product->urls->first()->slug])
@@ -57,10 +79,20 @@ class ProductPageTest extends TestCase
      */
     public function test_product_is_visible()
     {
+        $currency = Currency::factory()->create([
+            'default' => true,
+        ]);
+
         $product = Product::factory()
             ->hasUrls(1, [
                 'default' => true,
-            ])->has(ProductVariant::factory(), 'variants')
+            ])->has(ProductVariant::factory()->afterCreating(function ($variant) use ($currency) {
+                $variant->prices()->create(
+                    Price::factory()->make([
+                        'currency_id' => $currency->id,
+                    ])->getAttributes()
+                );
+            }), 'variants')
             ->create();
 
         Livewire::test(ProductPage::class, ['slug' => $product->urls->first()->slug])
